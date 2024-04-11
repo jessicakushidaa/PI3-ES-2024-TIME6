0542578f313bcd34eb931c8d6ca722208458c646
brunocarottiperes <bruno_carotti@hotmail.com>
2024-04-11 10:35:48 -0300

    Inserção das function na index.ts
    adc pessoas e adc cartao

--- Firebase-functions/functions/src/index.ts
+++ Firebase-functions/functions/src/index.ts
@@ -174,6 +174,113 @@ export const addPessoa = functions
     return result;
   });
 
+
+  interface Cartao {
+    titularName: string;
+    cardNumber: string;
+    cvv: number;
+    dataVal: Date;
+}
+
+function camposPreenchidos(p: Cartao): number {
+    if (!p.titularName) {
+        return 1;
+    }
+    if (!p.cardNumber) {
+        return 2;
+    }
+    if (!p.cvv) {
+        return 3;
+    }
+    if (!p.dataVal) {
+        return 4;
+    }
+    return 0;
+}
+
+function validarTiposCartao(cartao: Cartao): string[] | null {
+    const camposInvalidos: string[] = [];
+
+    if (typeof cartao.titularName !== "string") {
+        camposInvalidos.push("titularName");
+    }
+    if (typeof cartao.cardNumber !== "string") {
+        camposInvalidos.push("cardNumber");
+    }
+    if (typeof cartao.cvv !== "number") {
+        camposInvalidos.push("cvv");
+    }
+    if (!(cartao.dataVal instanceof Date)) {
+        camposInvalidos.push("dataVal");
+    }
+
+    return camposInvalidos.length > 0 ? camposInvalidos : null;
+}
+
+function errorMessage(codigo: number): string {
+    switch (codigo) {
+        case 1:
+            return "Nome do titular do cartão não informado.";
+        case 2:
+            return "Número do cartão não informado.";
+        case 3:
+            return "CVV não informado";
+        case 4:
+            return "Data de validade não informado";
+        default:
+            return "";
+    }
+}
+
+export const addCartao = functions
+    .region("southamerica-east1")
+    .https.onCall(async (data, context) => {
+        let result: CallableResponse;
+
+        functions.logger.info("Function addCartao - Iniciada.");
+        
+        const cartao: Cartao = {
+            titularName: data.titularName,
+            cardNumber: data.cardNumber,
+            cvv: data.cvv,
+            dataVal: new Date(data.dataVal),
+        };
+
+        const CodigoErro = camposPreenchidos(cartao);
+        const MensagemErro = errorMessage(CodigoErro);
+        const camposInvalid = validarTiposCartao(cartao);
+
+        if (CodigoErro > 0) {
+            // gravar o erro no log e preparar o retorno.
+            functions.logger.error("addCartao " +
+              "- Erro ao inserir novo Cartao:" +
+              CodigoErro.toString()),
+      
+            result = {
+              status: "ERROR",
+              message: MensagemErro,
+              payload: JSON.parse(JSON.stringify({docId: null})),
+            };
+            console.log(result);
+        } else if (camposInvalid !== null && camposInvalid.length > 0) {
+            result = {
+              status: "ERROR",
+              message: `Os seguintes campos não correspondem ao tipo esperado:
+               ${camposInvalid.join(", ")}.`,
+              payload: JSON.parse(JSON.stringify({docId: null})),
+            };
+        } else {
+            const docRef = await colCartoes.add(cartao);
+            result = {
+              status: "SUCCESS",
+              message: "Cartao inserido com sucesso.",
+              payload: JSON.parse(JSON.stringify({docId: docRef.id.toString()})),
+            };
+            functions.logger.error("addCartaoTeste - Nova pessoa inserida");
+          }
+          return result;
+        });
+
 /* Função que retorna campos do documento solicitado do bd */
 export const getDocumentById = functions
   .region("southamerica-east1")
