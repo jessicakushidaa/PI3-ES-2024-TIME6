b99202ee45bd704b487e9c97fa53f8c64091a549
jessicakushidaa <jessicakushida2@gmail.com>
2024-04-13 16:36:52 -0300

    Restructure project functions directories according to best practices

---
+++ Firebase-functions/functions/src/models/card.ts
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+
+// Define a estrutura de dados de um cartão
+export interface Cartao {
+    nomeTitular: string; // Nome do titular do cartão
+    numeroCartao: string; // Número do cartão
+    dataVal: Date; // Data de validade do cartão
+}
