dda4ba4c64494d965bff11948148f46db549b0b4
Izabelle Oliveira <118742135+IzabelleOliveira@users.noreply.github.com>
2024-04-26 14:07:09 -0300

    seta,nav e minhaslocacoesLayout

--- AndroidStudio/app/src/main/java/com/example/pi_iii_grupo6/MinhasLocacoesActivity.kt
+++ AndroidStudio/app/src/main/java/com/example/pi_iii_grupo6/MinhasLocacoesActivity.kt
@@ -2,12 +2,15 @@ package com.example.pi_iii_grupo6
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pi_iii_grupo6.databinding.ActivityMinhasLocacoesBinding
 import com.example.pi_iii_grupo6.MainViewActivity.Companion.locacoesConfirmadas
 import com.example.pi_iii_grupo6.MainViewActivity.Companion.locacoesPendentes
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseUser
 import com.google.gson.Gson
 
 
@@ -18,10 +21,6 @@ class MinhasLocacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMinhasLocacoesBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-
-        var recyclerView = binding?.recyclerviewLocacoes
-
-        recyclerView?.layoutManager = LinearLayoutManager(this)
 
     checarLocacoes()
     }
@@ -36,6 +35,55 @@ class MinhasLocacoesActivity : AppCompatActivity() {
             Toast.makeText(baseContext,"Voce possui ${numLocs} locações",Toast.LENGTH_SHORT).show()
         }else{
             Toast.makeText(baseContext,"Voce NAO possui locações concluídas",Toast.LENGTH_SHORT).show()
+        }
+
+        //Seta Retorno
+        val toolbar : Toolbar = findViewById(R.id.toolbar) //achando id da toolbar
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true) //botão voltar na toolbar
+            setDisplayShowTitleEnabled(false) // Remove o texto do nome do aplicativo
+        }
+
+
+        //Declarando user como null, para depois atribuir o usuário do authenticator a ele (que pode ser null se for anonimo)
+        var user: FirebaseUser? = null
+
+        //Direcionando o bottomNavigation
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                //tela Locações
+                R.id.page_locacoes -> {
+                    if (user != null) {
+                        startActivity(Intent(this, RentManagerActivity::class.java))
+                    }else{
+                        Toast.makeText((baseContext), "Faça login para acessar essa função",Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    }
+                    true
+                }
+                //tela Mapa
+                R.id.page_mapa -> {
+                    startActivity(Intent(this, MainViewActivity::class.java))
+                    true
+                }
+                //tela Cartões
+                R.id.page_cartoes -> {
+                    if (user != null) {
+                        startActivity(Intent(this, ShowCardActivity::class.java))
+                    }else{
+                        Toast.makeText((baseContext),"Faça login para acessar essa função",Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    }
+                    true
+                }
+
+                else -> false
+
+            }
         }
 
     }
