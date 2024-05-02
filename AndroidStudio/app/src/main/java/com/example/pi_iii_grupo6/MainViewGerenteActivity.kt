fbc9b6bf2bf960decb47da9d120beaa02a7912aa
Caio Acosta <caioacosta4@gmail.com>
2024-05-02 09:00:25 -0300

    Login Gerente

---
+++ AndroidStudio/app/src/main/java/com/example/pi_iii_grupo6/MainViewGerenteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pi_iii_grupo6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainViewGerenteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_view_gerente)
+    }
+}
