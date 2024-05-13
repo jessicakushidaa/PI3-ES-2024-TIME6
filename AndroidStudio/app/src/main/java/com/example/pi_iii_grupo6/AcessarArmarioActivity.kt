927ed79b352e4bd737f610ef89ee95bdcb340b57
Caio Acosta <caioacosta4@gmail.com>
2024-05-13 14:03:11 -0300

    Mudanca de Activity
    Transferência de activity para ler o qr Code

--- AndroidStudio/app/src/main/java/com/example/pi_iii_grupo6/AcessarArmarioActivity.kt
+++ AndroidStudio/app/src/main/java/com/example/pi_iii_grupo6/AcessarArmarioActivity.kt
@@ -15,67 +15,10 @@ import com.journeyapps.barcodescanner.ScanOptions
 
 
 class AcessarArmarioActivity : AppCompatActivity() {
-    var binding: ActivityAcessarArmarioBinding? = null
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()){
-            isGranted: Boolean ->
-            if (isGranted){
-                openCamera()
-            }else{
-                //Explicar o motivo das permissões
-            }
-        }
-
-    private val scanLauncher =
-        registerForActivityResult(ScanContract()){result: ScanIntentResult ->
-            run {
-                if (result.contents == null) {
-                    Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
-                } else {
-                    setResult(result.contents)
-                }
-            }
-            }
+    private var binding: ActivityAcessarArmarioBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAcessarArmarioBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-
-        checkPermissionCamera(this)
     }
-
-    //Função que recebe o conteúdo do codigo Qr e Passa para a variável resultado
-    private fun setResult(string: String){
-            resultado = string
-            binding?.tvArmarioAcessar?.text = resultado
-    }
-
-    //Função que checa as permissões da câmera do usuário
-    private fun checkPermissionCamera(context: android.content.Context) {
-        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
-            openCamera()
-        }else if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)){
-            Toast.makeText(context, "Permissões para Câmera necessárias", Toast.LENGTH_SHORT).show()
-        }else{
-            requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
-        }
-    }
-
-    //Função que chama a camera do dispositivo
-    private fun openCamera() {
-        val options = ScanOptions()
-        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
-        options.setPrompt("Scan QR code")
-        options.setCameraId(0)
-        options.setBeepEnabled(false)
-        options.setBarcodeImageEnabled(false)
-        options.setOrientationLocked(false)
-
-        scanLauncher.launch(options)
-    }
-
-    companion object{
-        lateinit var resultado: String
-    }
-
 }
