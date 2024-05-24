a94f76cd8a2e66ca49354b8b96b7b83589dc394b
Caio Acosta <caioacosta4@gmail.com>
2024-05-24 17:36:43 -0300

    log e bugfix

--- AndroidStudio/app/src/main/java/com/example/pi_iii_grupo6/MinhasLocacoesActivity.kt
+++ AndroidStudio/app/src/main/java/com/example/pi_iii_grupo6/MinhasLocacoesActivity.kt
@@ -10,7 +10,6 @@ import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.ProgressBar
 import android.widget.Toast
